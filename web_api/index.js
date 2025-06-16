4368d9b90fe30f83e9de968f9d6dda964f0e5393
Karl MARQUES <karl@kmarques.dev>
2025-06-16 09:18:09 +0200

    web api + hashRouter

--- web_api/index.js
+++ web_api/index.js
@@ -37,30 +37,34 @@ for (let rowIndex = 0; rowIndex < 5; rowIndex++) {
   }
 }
 
-root.appendChild(table);
-
 const tableStructure = {
-  tag: "table",
-  attributes: [
-    ["id", "table1"],
-    ["style", { backgroundColor: "magenta", color: "yellow" }],
-  ],
+  tag: "div",
   children: [
+    Link({ link: "/gallery", title: "Gallery" }),
     {
-      tag: "tbody",
-      attributes: [["class", "tbody-class"]],
-      children: Array.from({ length: 5 }, function createRow(_, rowIndex) {
-        return {
-          tag: "tr",
-          children: Array.from({ length: 5 }, (_, colIndex) => ({
-            tag: "td",
-            events: {
-              click: [onClick],
-            },
-            children: ["Default"],
-          })),
-        };
-      }),
+      tag: "table",
+      attributes: [
+        ["id", "table1"],
+        ["style", { backgroundColor: "magenta", color: "yellow" }],
+      ],
+      children: [
+        {
+          tag: "tbody",
+          attributes: [["class", "tbody-class"]],
+          children: Array.from({ length: 5 }, function createRow(_, rowIndex) {
+            return {
+              tag: "tr",
+              children: Array.from({ length: 5 }, (_, colIndex) => ({
+                tag: "td",
+                events: {
+                  click: [onClick],
+                },
+                children: ["Default"],
+              })),
+            };
+          }),
+        },
+      ],
     },
   ],
 };
@@ -102,10 +106,16 @@ function generateStructure(structure) {
 function Gallery() {
   return {
     tag: "div",
-    children: Array.from({ length: 500 }, (_, index) => ({
-      tag: "img",
-      attributes: [["src", "https://picsum.photos/200?random=" + index]],
-    })),
+    children: [
+      Link({ link: "/home", title: "HomePage" }),
+      {
+        tag: "div",
+        children: Array.from({ length: 500 }, (_, index) => ({
+          tag: "img",
+          attributes: [["src", "https://picsum.photos/200?random=" + index]],
+        })),
+      },
+    ],
   };
 }
 async function GalleryDatabase() {
@@ -157,6 +167,43 @@ function createElement(tag, attributes, ...children) {
   };
 }
 
-root.appendChild(generateStructure(tableStructure));
+const page404 = {
+  tag: "h1",
+  children: ["Tu t'es perdu !!! Game Over !!!"],
+};
+
+const routes = {
+  "/home": tableStructure,
+  "/gallery": Gallery(),
+  "*": page404,
+};
+
+function HashRouter(routes, rootElement) {
+  function generatePage() {
+    const path = window.location.hash.slice(1);
+    const struct = routes[path] ?? routes["*"];
+    const page = generateStructure(struct);
+    if (rootElement.childNodes.length === 0) rootElement.appendChild(page);
+    else rootElement.replaceChild(page, rootElement.childNodes[0]);
+  }
+
+  window.addEventListener("hashchange", generatePage);
+  generatePage();
+}
+
+function Link(props) {
+  const link = props.link;
+  const title = props.title;
+
+  return {
+    tag: "a",
+    attributes: [["href", "#" + link]],
+    children: [title],
+  };
+}
+
+HashRouter(routes, root);
+
+/* root.appendChild(generateStructure(tableStructure));
 root.appendChild(generateStructure(Toto()));
-//(async () => root.appendChild(generateStructure(await GalleryDatabase())))();
+ */ //(async () => root.appendChild(generateStructure(await GalleryDatabase())))();
