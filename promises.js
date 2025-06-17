e52e7bae55943e5b25f4ddcca5727b84403cf7fe
Karl MARQUES <karl@kmarques.dev>
2025-06-17 12:30:47 +0200

    exemple promise

--- promises.js
+++ promises.js
@@ -1,52 +1,87 @@
-const mail = {
-  recipientEmail: "toto@toto.com",
-  recipientName: "toto",
-  senderEmail: "titi@titi.com",
-  senderName: "Titi",
-  subject: "Promise is wonderful",
-  content: "Promise is very wonderful",
-};
-
-const campaign = {
-  name: "Promise promotion",
-  mail: mail,
-};
-
-const campaignBatch = {
-  name: "Promise promotion",
-  mails: [mail, mail, mail, mail],
-};
-
-function createCampaignMail(mailData) {
-  try {
-    validateMailData(mailData);
-    dbConnect()
-      .then(() => insertCampaign(campaign))
-      .then(fetchCampaign)
-      .then((campaignData) => sendMail(campaign.mail))
-      .then(() => send(201))
-      .catch((error) => {
-        console.error(error.message);
-        send(500);
-      });
-  } catch (error) {
-    console.error(error.message);
-  }
+/*
+Simulation d’appels serveurs
+Créer une Promise getStudents qui récupère une liste d’étudiants entre 1 et 2 secondes
+EX: [ { name: "Dupont", cours: [ 1, 3, 5 ] }, { name: "Lea", cours: [ 2, 4 ] }, { name: "Charles", cours: [ 1 ] } ]
+Créer une Promise getCourses qui récupère une liste de cours entre 2 et 4 secondes
+EX: [ { id: 1, name: "JS" }, { id: 2, name: "PHP" }, { id: 3, name: "C#" }, { id: 4, name: "F#" }, { id: 5, name: "CSS" } ]
+Créer une Promise qui mappe à l’ensemble des étudiants les cours associés entre 1 et 4 secondes
+EX: [ { name: "Lea", cours: [ { id: 2, name: "PHP" }, { id: 4, name: "F#" } ] }, … ]
+Créer une Promise qui contrôle le temps d’accès global
+Celle-ci doit rejeter si le temps max dépasse 7 secondes
+Afficher la fonction et le temps estimé pour chaque Promise
+EX: "getStudents:2"
+Afficher "Merge OK" si tout s’est bien passé sinon "Timeout"
+*/
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function createBatchCampaignMail(mailData) {
-  try {
-    validateMailData(mailData);
-    dbConnect()
-      .then(() => insertCampaign(campaign))
-      .then(fetchCampaign)
-      .then((campaignData) => Promise.all(campaignBatch.mails.map(sendMail)))
-      .then(() => send(201))
-      .catch((error) => {
-        console.error(error.message);
-        send(500);
-      });
-  } catch (error) {
-    console.error(error.message);
-  }
+function getStudents() {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      resolve([
+        { name: "Dupont", cours: [1, 3, 5] },
+        { name: "Lea", cours: [2, 4] },
+        { name: "Charles", cours: [1] },
+      ]);
+    }, randomInt(1, 2) * 1000);
+  });
 }
+function getCourses() {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve([
+        { id: 1, name: "JS" },
+        { id: 2, name: "PHP" },
+        { id: 3, name: "C#" },
+        { id: 4, name: "F#" },
+        { id: 5, name: "CSS" },
+      ]);
+    }, randomInt(2, 4) * 1000);
+  });
+}
+
+function mapStudents(students, courses) {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve(
+        students.map((student) => {
+          student.cours = student.cours.map((coursId) =>
+            courses.find((cours) => cours.id === coursId)
+          );
+
+          return student;
+        })
+      );
+    }, randomInt(1, 4));
+  });
+}
+
+function main() {
+  return Promise.all([getStudents(), getCourses()])
+    .then((result) => {
+      const students = result[0];
+      const courses = result[1];
+      return mapStudents(students, courses);
+    })
+    .then((result) => {
+      console.log(result);
+      // return result
+    });
+}
+
+function timer() {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      //resolve();
+      reject();
+    }, 7 * 1000);
+  });
+}
+
+Promise.race([main(), timer()])
+  .then(() => {
+    //if (result === undefined) return console.log("Timeout");
+    console.log("Merge OK");
+  })
+  .catch(() => console.log("Timeout"));
