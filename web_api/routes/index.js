8f616239bff4e872d802c5601a0f210d30663976
thomas.sansberro <sansberrot@gmail.com>
2025-07-22 15:29:28 +0200

    doit encore modifier le truc avec les communautés

--- web_api/routes/index.js
+++ web_api/routes/index.js
@@ -1,4 +1,5 @@
-import Evenement from "../views/Evenement.js";
+import EvenementPage from "../views/EvenementPage.js";
+import EvenementCreate from "../views/EvenementCreate.js";
 import Gallery from "../views/GalleryPage.js";
 import Page404 from "../views/Page404.js";
 import TablePage from "../views/TablePage.js";
@@ -11,8 +12,27 @@ export default {
     tag: Gallery,
   },
   "/evenement": {
-    tag: Evenement,
+    tag: EvenementPage,
   },
+  // "/evenement/:id": async () => {
+  //   const pathParts = window.location.pathname.split('/');
+  //   const eventId = pathParts[pathParts.length - 1];
+  //   return {
+  //     tag: EvenementDetail,
+  //     attributes: [["eventId", eventId]],
+  //   };
+  // },
+  "/evenement/create": {
+    tag: EvenementCreate,
+  },
+  // "/evenement/edit/:id": async () => {
+  //   const pathParts = window.location.pathname.split('/');
+  //   const eventId = pathParts[pathParts.length - 1];
+  //   return {
+  //     tag: EvenementEdit,
+  //     attributes: [["eventId", eventId]],
+  //   };
+  // },
   "*": {
     tag: Page404,
   },
